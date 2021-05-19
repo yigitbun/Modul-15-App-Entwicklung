482b08ec5d938bca198de54c8cf55b65368dbda9
Bunyamin Yigit <yigitbun@gmail.com>
2021-05-19 16:33:22 +0200

    update

--- Cordova-Plugin_Project/qrscanner/www/js/index.js
+++ Cordova-Plugin_Project/qrscanner/www/js/index.js
@@ -23,7 +23,72 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
-
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+
+    function onDone(err, status){
+        if (err) {
+            console.error(err);
+            // here we can handle errors and clean up any loose ends.
+        }
+        if (status.authorized) {
+            console.log('authorized');
+            QRScanner.show();
+            QRScanner.scan(displayContents);
+            // W00t, you have camera access and the scanner is initialized.
+            // QRscanner.show() should feel very fast.
+
+        } else if (status.denied) {
+            console.log('denied');
+            // The video preview will remain black, and scanning is disabled. We can
+            // try to ask the user to change their mind, but we'll have to send them
+            // to their device settings with `QRScanner.openSettings()`.
+        } else {
+            console.log('error');
+            // we didn't get permission, but we didn't get permanently denied. (On
+            // Android, a denial isn't permanent unless the user checks the "Don't
+            // ask again" box.) We can ask again at the next relevant opportunity.
+        }
+    }
+
+    let scanBtn = document.getElementById('scan');
+
+    scanBtn.addEventListener('click', function() {
+        QRScanner.prepare(onDone); // show the prompt
+    });
+
+    function displayContents(err, text){
+        if(err){
+            // an error occurred, or the scan was canceled (error code `6`)
+            console.log('error', err);
+        } else {
+            // The scan completed, display the contents of the QR code:
+            console.log(text);
+            document.getElementById('output').textContent = text.result;
+        }
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    var qrcode = new QRCode("qrcode");
+    
+    function makeCode () {    
+        var elText = document.getElementById("text");
+        
+        if (!elText.value) {
+            qrcode.makeCode('');
+            return;
+        }
+        qrcode.makeCode(elText.value);
+    }
+    
+    makeCode();
+
+    let input = document.getElementById('text');
+    input.addEventListener('input', function(event) {
+        makeCode();
+    });
 }
